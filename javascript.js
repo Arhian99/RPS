d32e648df4bcbaef64443de5effd07ab76dcf664
Arhian Albis Ramos <108996128+Arhian99@users.noreply.github.com>
2022-08-21 13:13:12 -0500

    implement check winner function that takes a player selection and a random computer selection and returns a string identifying the winner

--- javascript.js
+++ javascript.js
@@ -1,10 +1,36 @@
 
 
-let choices = ['Rock', 'Paper', 'Scissors']
+let choices = ['rock', 'paper', 'scissors']
+let computerSelection
 
 function getComputerChoice() {
-    computerChoice = choices[Math.floor(Math.random() * choices.length)];
-    return computerChoice;
+    computerSelection = choices[Math.floor(Math.random() * choices.length)];
+    return computerSelection;
 }
 
-console.log(getComputerChoice());
+// must implement a way to choose/ decide playerSelection
+computerSelection = getComputerChoice();
+
+function checkWinner(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection) {
+        return 'Tie!, Play Again!'
+    }
+
+    else if (playerSelection === 'rock' && computerSelection === 'paper' || playerSelection === 'paper' && computerSelection === 'scissors' || playerSelection === 'scissors' && computerSelection === 'rock') {
+        return `You Loose! ${caps2(computerSelection)} beats ${caps2(playerSelection)}`
+    }
+
+    else {
+        return `You Win! ${caps2(playerSelection)} beats ${caps2(computerSelection)}`
+    }    
+}
+
+function caps2(string) {
+    let letter1 = string.charAt(0);
+    letter1 = letter1.toUpperCase();
+    let rest = string.slice(1, string.length);
+    rest = rest.toLowerCase();
+    return letter1 + rest;
+}
