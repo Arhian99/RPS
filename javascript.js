c5ff870093f4c1cea327f9724c139ea292133b0a
Arhian Albis Ramos <108996128+Arhian99@users.noreply.github.com>
2022-08-22 20:59:26 -0500

    allow user to input their choice and implement scoring system and best out of 5 game system

--- javascript.js
+++ javascript.js
@@ -1,5 +1,3 @@
-
-
 let choices = ['rock', 'paper', 'scissors']
 let computerSelection
 
@@ -9,23 +7,6 @@ function getComputerChoice() {
 }
 
 // must implement a way to choose/ decide playerSelection
-computerSelection = getComputerChoice();
-
-function checkWinner(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-
-    if (playerSelection === computerSelection) {
-        return 'Tie!, Play Again!'
-    }
-
-    else if (playerSelection === 'rock' && computerSelection === 'paper' || playerSelection === 'paper' && computerSelection === 'scissors' || playerSelection === 'scissors' && computerSelection === 'rock') {
-        return `You Loose! ${caps2(computerSelection)} beats ${caps2(playerSelection)}`
-    }
-
-    else {
-        return `You Win! ${caps2(playerSelection)} beats ${caps2(computerSelection)}`
-    }    
-}
 
 function caps2(string) {
     let letter1 = string.charAt(0);
@@ -33,4 +14,55 @@ function caps2(string) {
     let rest = string.slice(1, string.length);
     rest = rest.toLowerCase();
     return letter1 + rest;
+} // this function takes the first letter of a string and capitalizes it, used to capitalize the returned strings
+
+let playerScore = 0;
+let computerScore = 0;
+let playerSelection = prompt('Rock, Paper, Scissors?');
+let tiedRound;
+
+function checkWinner(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection) {
+        tiedRound = true;
+        return 'Tie!, Play Again!';
+    }
+
+    else if (playerSelection === 'rock' && computerSelection === 'paper' || playerSelection === 'paper' && computerSelection === 'scissors' || playerSelection === 'scissors' && computerSelection === 'rock') {
+        computerScore += 1;
+        tiedRound = false;
+        return `You Loose this Round! ${caps2(computerSelection)} beats ${caps2(playerSelection)}`;
+    }
+
+    else if (playerSelection === 'rock' && computerSelection === 'scissors' || playerSelection === 'paper' && computerSelection === 'rock' || playerSelection === 'scissors' && computerSelection === 'paper') {
+        playerScore += 1;
+        tiedRound = false;
+        return `You Win this Round! ${caps2(playerSelection)} beats ${caps2(computerSelection)}`;
+    }
 }
+
+
+function game() {
+    for (let i=0; i < 5; i++) {
+        computerSelection = getComputerChoice();
+        console.log(checkWinner(playerSelection, computerSelection));
+        if (tiedRound === true) {i--}
+        console.log(`Player: ${playerScore} vs Computer: ${computerScore}`)
+    }
+
+    if (playerScore > computerScore) {
+        console.log(`You Win the Game!!! Final Score: Player - ${playerScore} vs Computer - ${computerScore}`);
+    }
+
+    else if (computerScore > playerScore) {
+        console.log(`You Loose the Game!!! Final Score: Player: ${playerScore} vs Computer: ${computerScore}`);
+    }
+
+    else {
+        console.log(`Tied Game! Play Again! Final Score: Player: ${playerScore} vs Computer: ${computerScore} `)
+    }
+} 
+
+console.log(game());
+
